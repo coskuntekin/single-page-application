6689f3b68f968042498be7770b4c467d000aaa73
coskuntekin <coskuntekin@msn.com>
2024-05-30 20:08:46 +0800

    chore: Update NotFoundView.vue with improved styling and router link

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
-  <div>
+  <div
+    class="bg-[#041536] flex items-center flex-col justify-center h-screen w-full text-center text-white"
+  >
     <h1>404 Not Found</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
+    <p class="mb-8">Sorry, the page you are looking for does not exist.</p>
+    <RouterLink
+      to="/"
+      class="border border-slate-600 px-4 py-5 rounded hover:text-slate-700 transition-colors"
+      >Go back to the homepage</RouterLink
+    >
   </div>
 </template>
